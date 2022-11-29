3b70902e868e975f3fc98ec191cc3fad18f89c05
Rémi Espié <remi.espie@etu.umontpellier.fr>
2022-11-29 23:22:36 +0100

    database seems to be working in docker ? Added 1st service for students

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsService } from './students/students.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, StudentsService],
 })
 export class AppModule {}
