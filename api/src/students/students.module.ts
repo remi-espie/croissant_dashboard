eaf3c729bfd0ba0ad6513369dc8cb366f3bcbb40
Rémi Espié <remi.espie@etu.umontpellier.fr>
2022-11-30 23:26:54 +0100

    fix: forgot to add modules, now it is working

---
+++ api/src/students/students.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { StudentsController } from "./studentsController";
+import { StudentsService } from "./students.service";@Module({
+    providers: [StudentsService, PrismaService],
+    exports: [StudentsService],
+    controllers: [StudentsController],
+})
+export class StudentsModule {}
