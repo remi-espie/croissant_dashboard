579b8fa5f28ac9ab06408270d8867b4c6c0d4315
ESPIE <remi.espie@etu.umontpellier.fr>
2022-12-12 18:29:16 +0100

    fix: forgot to add students.dto.ts

--- api/src/students/students.dto.ts
+++ api/src/students/students.dto.ts
@@ -24,64 +24,19 @@ export class StudentsDto{
     public pastryId: string
 }
 
-export class StudentsDtoPassword{
+export class StudentsDtoId extends StudentsDto{
+    constructor(id, student: StudentsDto) {
+        super();
+        this.id = id
+        this.name = student.name
+        this.firstname = student.firstname
+        this.mail = student.mail
+        this.birthday = student.birthday
+        this.pastryId = student.pastryId
+        this.promotionId = student.promotionId
+    }
 
     @IsString()
     @IsNotEmpty()
     public id: string
-
-    @IsString()
-    @IsNotEmpty()
-    public name: string
-
-    @IsString()
-    @IsNotEmpty()
-    public firstname: string
-
-    @IsDateString()
-    @IsOptional()
-    public birthday: Date
-
-    @IsString()
-    @IsNotEmpty()
-    public mail: string
-
-    @IsNotEmpty()
-    public promotionId: string
-
-    @IsNotEmpty()
-    public pastryId: string
-
-    @IsNotEmpty()
-    @IsString()
-    public password: string
-}
-
-export class StudentsDtoId{
-    @IsString()
-    @IsNotEmpty()
-    public id: string
-
-    @IsString()
-    @IsNotEmpty()
-    public name: string
-
-    @IsString()
-    @IsNotEmpty()
-    public firstname: string
-
-    @IsDateString()
-    @IsOptional()
-    public birthday: Date
-
-    @IsString()
-    @IsNotEmpty()
-    public mail: string
-
-    @IsNotEmpty()
-    public promotionId: string
-
-    @IsNotEmpty()
-    public pastryId: string
-
 }
