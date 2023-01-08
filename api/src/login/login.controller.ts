d6d32e09967a7be40471d504e74ea9081dd77538
Rémi Espié <rem-e.84@orange.fr>
2023-01-08 13:33:39 +0100

    Revert "fix(backend login): hash password on password update before saving it to database"
    This reverts commit d51c4771c03cdf45bc5787ed361d565b3189b986.

--- api/src/login/login.controller.ts
+++ api/src/login/login.controller.ts
@@ -13,13 +13,20 @@ import {LoginService} from "./login.service";
 import {LoginDto, LoginDtoId} from "./login.dto";
 import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 import {StudentGuard} from "../auth/student-guard";
-import {PasswordProvider} from "../provider/password";
 
 @Controller("login")
 export class LoginController {
-    constructor(private readonly loginService: LoginService, private readonly passwordProvider: PasswordProvider) {
+    constructor(private readonly loginService: LoginService) {
     }
 
+    // // Create login -> POST /login
+    // @Post()
+    // async createLogin(
+    //     @Body() loginData: LoginDto
+    // ): Promise<LoginDto> {
+    //     return this.loginService.createLogin(loginData);
+    // }
+    //
     // Get login data -> GET /login/:id
     @Get()
     @UseGuards(JwtAuthGuard, StudentGuard)
@@ -27,6 +34,12 @@ export class LoginController {
     async profile(@Req() request): Promise<login> {
         return request.user;
     }
+    //
+    // // Get all logins data
+    // @Get("/all")
+    // async getAllLogin() {
+    //     return this.loginService.getAllLogin();
+    // }
 
     // Update login data -> PATCH /login/:id
     @UseGuards(JwtAuthGuard, StudentGuard)
@@ -36,7 +49,6 @@ export class LoginController {
         @Body() loginData: LoginDto,
         @Param("id") id
     ): Promise<LoginDto> {
-        loginData.password = await this.passwordProvider.hashPassword(loginData.password)
         const login = new LoginDtoId(loginData, id);
         return this.loginService.updateLogin(login);
     }
